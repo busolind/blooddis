96a211ce258f31b8255d92483d088de18caef683
Davide Busolin <davide.busolin@studenti.unimi.it>
2022-04-25 12:38:03 +0200

    Prediction on page

--- websrv/static/js/scripts.js
+++ websrv/static/js/scripts.js
@@ -1,4 +1,4 @@
-var form = document.getElementById("imguploadform");
+var form = document.getElementById('imguploadform');
 
 form.onsubmit = async (e) => {
   e.preventDefault();
@@ -8,13 +8,22 @@ form.onsubmit = async (e) => {
   try {
     const formData = new FormData(form);
     const response = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       body: formData,
     });
     response.json().then((data) => {
       console.log(data);
+      var div = document.getElementById('result');
+      div.classList.remove('d-none');
+      div.innerHTML = '';
+      var innerp = document.createElement('h2');
+      innerp.innerText = 'Your prediction is: ' + data['prediction'];
+      innerp.classList.add('text-info');
+      innerp.classList.add('animate__animated', 'animate__bounceInLeft');
+      div.appendChild(innerp);
     });
   } catch (error) {
     console.error(error);
+    alert('Prediction could not be made');
   }
 };
