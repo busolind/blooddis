dc9fda4bf719af5efa3e2bc5904bcefa885e27a5
Davide Busolin <davide.busolin@studenti.unimi.it>
2022-05-08 11:54:40 +0200

    Legal page + prettier intervention

--- websrv/projsrv.py
+++ websrv/projsrv.py
@@ -35,7 +35,8 @@ import numpy as np
 import json
 import os
 from datetime import datetime
-import random, string
+import random
+import string
 
 # Change this to serve on a different port
 PORT = 44444
@@ -43,15 +44,19 @@ PORT = 44444
 model = tf.keras.models.load_model('../q1modelg.hdf5')
 class_names = ['EOSINOPHIL', 'LYMPHOCYTE', 'MONOCYTE', 'NEUTROPHIL']
 
+
 class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self): #static content
-        root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-        #print(root)
+    def do_GET(self):  # static content
+        root = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), 'static')
+        # print(root)
         if self.path == '/':
             filename = root + '/index.html'
+        elif self.path == '/legal':
+            filename = root + '/legal.html'
         else:
             filename = root + self.path
- 
+
         self.send_response(200)
         if filename[-4:] == '.css':
             self.send_header('Content-type', 'text/css')
@@ -75,14 +80,14 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         f = io.BytesIO()
         if r:
             self.send_response(200)
-            outobj = {"prediction":pred}
+            outobj = {"prediction": pred}
             f.write(bytes(json.dumps(outobj), 'ascii'))
         else:
             self.send_response(500)
             f.write(b"Failed\n")
         length = f.tell()
         f.seek(0)
-        
+
         self.send_header("Content-type", "text/plain")
         self.send_header("Content-Length", str(length))
         self.end_headers()
@@ -98,34 +103,40 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         if ctype == 'multipart/form-data':
             form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={
                                     'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type'], })
-            #print(type(form))
+            # print(type(form))
             fileobj = form["file"]
             #readfile = fileobj.file.read()
             try:
                 npimg = np.fromfile(fileobj.file, np.uint8)
                 image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)#converting BGR image to RGB
-                image_size = (120,120)
-                image = cv2.resize(image, image_size)#resizing the input image acc to model input size
+                # converting BGR image to RGB
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image_size = (120, 120)
+                # resizing the input image acc to model input size
+                image = cv2.resize(image, image_size)
                 image = image.astype('float32')
                 image /= 255
                 image = np.expand_dims(image, axis=0)
                 pred = np.argmax(model.predict(image))
             except:
                 return False, ''
-            try: #Image saving part
+            try:  # Image saving part
                 if form.getvalue('shareImageCheckbox') == 'on':
-                    root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploaded')
+                    root = os.path.join(os.path.dirname(
+                        os.path.abspath(__file__)), 'uploaded')
                     if not os.path.isdir(root):
                         os.makedirs(root)
-                    fn = str(int(datetime.timestamp(datetime.now()))) + '-' + ''.join(random.choices(string.ascii_letters + string.digits, k=8)) + os.path.splitext(fileobj.filename)[1] #integer timestamp + 8 random chars + extension
+                    fn = str(int(datetime.timestamp(datetime.now()))) + '-' + ''.join(random.choices(string.ascii_letters +
+                                                                                                     string.digits, k=8)) + os.path.splitext(fileobj.filename)[1]  # integer timestamp + 8 random chars + extension
                     fileobj.file.seek(0)
-                    open(os.path.join(root, fn), 'wb').write(fileobj.file.read())
+                    open(os.path.join(root, fn), 'wb').write(
+                        fileobj.file.read())
                     #cv2.imwrite(os.path.join(root, fn), image)
             except Exception as e:
                 print(e)
                 return False, ''
-        else: return False, ''
+        else:
+            return False, ''
         return (True, class_names[pred])
 
 
@@ -133,5 +144,3 @@ Handler = CustomHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
-
-    
